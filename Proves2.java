3e60cf970779aebdfa23bb1a78b41bef505eb607
AlexVidalEstudillo <avidal2@inscaparrella.cat>
2023-02-23 19:29:10 +0100

    2n commit main

---
+++ Proves2.java
@@ -0,0 +1,19 @@
+package Rimador;
+
+public class Proves {
+    public static void main(String[] args) {
+        if(NouPc.calcularDies(1345, 900)==45){
+            System.out.println("true");
+        } else{
+            System.out.println("false");
+        }if(NouPc.calcularDies(1732, 566)==92){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }if(NouPc.calcularDies(2345, 1500)==46){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
